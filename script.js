45de7470244d6c02a48083c05353b3f59fc92a36
wlgus5704 <wlgus5704@dgu.ac.kr>
2024-01-16 06:53:58 +0900

    Nav Fin

--- script.js
+++ script.js
@@ -1,6 +1,50 @@
 window.onload = function(){
 
     //nav
+    let navlist = document.querySelector('nav>ul');
+
+    navlist.addEventListener("mouseover", function(){
+        navlist.querySelectorAll('.submenu').forEach(function(nav){
+            nav.style.height = '327px';
+        });
+        document.getElementById('header').classList.add('bgon');
+
+    });
+
+
+    navlist.addEventListener("mouseout", function(){
+        navlist.querySelectorAll('.submenu').forEach(function(nav){
+            nav.style.height = '0px';
+            nav.style.transition = '0.3s';
+        });
+        document.getElementById('header').classList.remove('bgon');
+    });
+
+    //nav color
+    let navcolor = document.querySelectorAll('nav>ul>li');
+
+
+    navcolor.forEach(function(nc){
+        nc.addEventListener('mouseover', function(){
+            nc.querySelector('a').style.borderBottom="3px solid var(--gray_100)";
+            nc.querySelectorAll('ul>li>a').forEach(function(titles){
+                titles.style.color='var(--gray_100)';
+            });
+        }); 
+    });
+
+    navcolor.forEach(function(nc){
+        nc.addEventListener('mouseout', function(){
+            nc.querySelector('a').style.borderBottom="none";
+            nc.querySelectorAll('ul>li>a').forEach(function(titles){
+                titles.style.color='var(--gray_300)';
+            });
+        }); 
+    });
+   
+
     
+
+
 
 };
